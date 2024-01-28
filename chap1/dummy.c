7431eef177b602df66b8cb888e32c3495081f326
root <root@DESKTOP-KJC0TQ2.localdomain>
2024-01-28 19:06:26 +0900

    add: practice files and image of chapter1

---
+++ chap1/dummy.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// main should return int
+int main(){
+	/*
+	this is a dummy code!
+	*/
+}
--- chap1/remove_comment.c
+++ chap1/remove_comment.c
@@ -1,44 +1,54 @@
-#inlcude <stdio.h> 
+#include <stdio.h> 
+#include <stdbool.h>
 
-#define MAX_LENGTH = 1000; 
+#define MAX_LENGTH 1000 
 
 int main(){
-  char c;
-  char next_c;
-  char content[MAX_LENGTH];
-  bool single_line_comment = false;
-  bool muti_line_comment = false;
-  int i = 0;
-  
-  while((c = getchar()) != EOF){
-    if(c == '/'){
-      next_c = getchar();
-      if(next_c == '/'){
-        single_line_comment = true;
-        continue;
-      if(next_c == '*'){
-        multi_line_comment = true;
-        continue;
-      }
-      content[i] = c;
-      content[i + 2] = next_c;
-      i += 2;
-    }
-    if(single_line_comment){
-      if(c == '\n'){
-        single_line_comment = false;
-      }
-    } else if(multi_line_comment){
-      if(c == '*'){
-        next_c = getchar();
-        if(next_c == '/'){
-          multi_line_comment = false;
-        }
-      }
-    } else {
-      content[i] = c;
-      ++i;
-    } 
-  }
-  return content;
+	char c;
+	char next_c;
+	char content[MAX_LENGTH] = "";
+	bool single_line_comment = false;
+	bool multi_line_comment = false;
+	int i = 0;
+
+	while((c = getchar()) != EOF){
+		if(c == '/'){
+			next_c = getchar();
+			if (next_c == '/') {
+				single_line_comment = true;
+				continue;
+			}
+			if(next_c == '*'){
+				multi_line_comment = true;
+				continue;
+			}
+			content[i] = c;
+			content[i + 2] = next_c;
+			i += 2;
+			continue;
+		}
+		if (single_line_comment) {
+			if (c == '\n') {
+				single_line_comment = false;
+			}
+			continue;
+		}
+		if(multi_line_comment){
+			if(c == '*'){
+				next_c = getchar();
+				if(next_c == '/'){
+					multi_line_comment = false;
+				}
+			}
+			continue;
+		}
+		content[i] = c;
+		++i;
+	}
+	printf("\n");
+	for (i = 0; i < MAX_LENGTH; i++) {
+		printf("%c", content[i]);
+	}
+	printf("\n");
+	return 0;
 }
